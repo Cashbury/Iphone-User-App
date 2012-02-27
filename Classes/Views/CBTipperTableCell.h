b761fae57792496e5ced04dc1a6e4cff5033ca0b
ramikay <ramikhawandi@gmail.com>
2012-02-27 22:39:30 +0200

    Finished the tipper table

---
+++ Classes/Views/CBTipperTableCell.h
@@ -0,0 +1,13 @@
+//
+//  CBTipperTableCell.h
+//  Cashbury
+//
+//  Created by Rami on 27/2/12.
+//  Copyright (c) 2012 Cashbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBTipperTableCell : UITableViewCell
+
+@end
---
+++ Classes/Views/CBTipperTableCell.m
@@ -0,0 +1,29 @@
+//
+//  CBTipperTableCell.m
+//  Cashbury
+//
+//  Created by Rami on 27/2/12.
+//  Copyright (c) 2012 Cashbury. All rights reserved.
+//
+
+#import "CBTipperTableCell.h"
+
+@implementation CBTipperTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
